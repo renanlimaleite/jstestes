9926a8a66705c67663d165ef7ca67e12fd8ab336
renanlimaleite <renan.lima.softwares@gmail.com>
2022-04-07 11:49:38 -0300

    add p1

--- parte1/src/lib/calculator.js
+++ parte1/src/lib/calculator.js
@@ -1,3 +1,10 @@
 export function sum(num1, num2) {
+  num1 = Number(num1)
+  num2 = Number(num2)
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error('You should be pass all parameters and they are numbers.')
+  }
+  
   return num1 + num2
 }
--- parte1/src/lib/calculator.spec.js
+++ parte1/src/lib/calculator.spec.js
@@ -1,5 +1,27 @@
 import { sum } from './calculator'
 
 it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum(2, 2)).toBe(4)
+  expect(sum(2, 2)).toBe(4)  
+});
+
+it('should sum 2 and 2 even if one them is a string and the result must be 4', () => {
+  expect(sum('2', 2)).toBe(4)  
+});
+
+it('should throw an error if what provider to the method cannot be summed', () => {
+  expect(() => {
+    sun('', 2)
+  }).toThrowError()
+
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError()
+
+  expect(() => {
+    sum(1)
+  }).toThrowError()
+
+  expect(() => {
+    sum()
+  }).toThrowError()
 });
